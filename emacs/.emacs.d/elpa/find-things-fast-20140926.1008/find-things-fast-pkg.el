1ec454e7fd2a367d5af7ff6f9c94125fc0480624
zeptonaut <andrews.charlie@gmail.com>
2014-10-06 13:46:16 -0500

    Remove corrupted elpa.

--- emacs/.emacs.d/elpa/find-things-fast-20140926.1008/find-things-fast-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "find-things-fast" "20140926.1008" "Find things fast, leveraging the power of git" 'nil :keywords '("project" "convenience"))
--- emacs/.emacs.d/elpa/ido-ubiquitous-20140904.1547/ido-ubiquitous-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ido-ubiquitous" "20140904.1547" "Use ido (nearly) everywhere." '((emacs "24.1")) :url "https://github.com/DarwinAwardWinner/ido-ubiquitous" :keywords '("convenience" "completion" "ido"))
