1f03dba4471307e382ace60d1576c2e314713359
Charlie Andrews <charliea@google.com>
2015-03-27 14:52:55 -0700

    Added a few missing plugins

---
+++ emacs/.emacs.d/elpa/org-mobile-sync-20131118.1116/org-mobile-sync-pkg.el
@@ -0,0 +1,1 @@
+(define-package "org-mobile-sync" "20131118.1116" "automatically sync org-mobile on changes" '((emacs "24.3.50") (org "8.0")) :url "https://github.com/steckerhalter/org-mobile-sync" :keywords '("org-mode" "org" "mobile" "sync" "todo"))
---
+++ emacs/.emacs.d/elpa/zenburn-0.1/zenburn-autoloads.el
@@ -0,0 +1,15 @@
+;;; zenburn-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("zenburn.el") (21729 3835 784042 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; zenburn-autoloads.el ends here
