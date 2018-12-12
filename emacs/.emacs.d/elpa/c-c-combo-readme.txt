f814fdab3e10faf65b168db2d816fb72988529b2
Charlie Andrews <charliea@chromium.org>
2018-12-12 15:04:17 -0500

    Remove a whole bunch of useless files

--- emacs/.emacs.d/elpa/c-c-combo-readme.txt
+++
@@ -1,1 +0,0 @@
-This will make sounds appear after you hit more than 60 wpm
--- emacs/.emacs.d/elpa/seq-20151121.1017/seq-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; seq-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("seq.el") (22123 2618 707780 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; seq-autoloads.el ends here
